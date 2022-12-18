d447d842338d681aa32b888f6cd996854bd96e0f
Ashley Baldry <arbaldry91@gmail.com>
2022-12-18 10:05:06 +0000

    Converting + to enable older version of `save`

--- data-raw/apple_data.R
+++ data-raw/apple_data.R
@@ -1,25 +1,4 @@
-library(appler)
-
 # iTunes web --------------------------------------------------------------
-# https://music.apple.com/ca/browse
-# https://music.apple.com/ca/album/get-rollin/1642697894
-# https://music.apple.com/ca/artist/nickelback/5280361
-# https://music.apple.com/ca/artist/<artist-name>  fails
-
-# Browse Categories > Hits
-# https://music.apple.com/ca/curator/apple-music-hits/1526756058
-# Hot Tracks        https://music.apple.com/room/1532292351
-# New Releases      https://music.apple.com/room/1532292352
-# Artist Playlist   https://music.apple.com/room/1532292353
-# Hits of the 2010s https://music.apple.com/room/1532292355
-# Hits of the '00s  https://music.apple.com/room/1532292356
-# Hits of the 90s   https://music.apple.com/room/1532292357
-# Essential Albums  https://music.apple.com/room/1532292578
-
-# Browser Categories > Charts
-# https://music.apple.com/ca/browse/top-charts
-# Top Songs   https://music.apple.com/ca/browse/top-charts/songs/
-
 # --- sample of some artists for quick use for users
 itunes_artists <- data.frame(
   artist = c(
@@ -49,25 +28,10 @@ itunes_artists <- data.frame(
 )
 save(itunes_artists, file = "data/itunes_artists.rda", compress = "bzip2", version = 3)
 
-# --- use the dataset to get artist id
-lizzo <- itunes_artists[itunes_artists$artist == "Lizzo", ]
-lizzo_id <- lizzo$artist_id
-lizzo_name <- lizzo$artist
-
-# --- use the artist name and id from the dataset for search
-artist_by_name <- search_apple(term = lizzo_name, country = "ca", lang = "en")
-artist_by_id <- lookup_apple(id = lizzo_id, country = "ca", sort = "recent")
-
-# --- check the data
-head(artist_by_name)
-head(artist_by_id)
-
 # Apple app store ---------------------------------------------------------
-
 # Develop > Top Free Apps (iPhone)
 # ---- the id is the same if iPad is chosen, just the order or app popularity changes
 # ---- 210 apps mostly from Top App selections by category
-
 apple_apps <- data.frame(
   app_name = c(
     "Xcode",
@@ -101,7 +65,7 @@ apple_apps <- data.frame(
     "Shortcuts",
     "Clips",
     "iTunes Movie Trailers",
-    "Beats Pill⁺",
+    "Beats Pill+",
     "Texas Hold’em",
     "App Store Connect",
     "Logic Remote",
@@ -495,19 +459,4 @@ apple_apps <- data.frame(
     1411869974
   )
 )
-save(apple_apps, file = "data/apple_apps.rda", compress = "bzip2", version = 3)
-
-# https://apps.apple.com/ca/developer/github/id429758986
-# https://apps.apple.com/ca/app/github/id1477376905
-
-# id of app on Apple App store
-# -- use the dataset apple_apps to get ids
-head(apple_apps)
-xcode <- apple_apps[apple_apps$app_name == "Xcode", ]
-
-get_apple_chart_postion(id = xcode$app_id, "ca")
-
-get_apple_rating_split(id = xcode$app_id, country = "ca")
-
-app_reviews <- get_apple_reviews( xcode$app_id, "ca", all_results = TRUE)
-head(app_reviews)
+save(apple_apps, file = "data/apple_apps.rda", compress = "bzip2", version = 2)
